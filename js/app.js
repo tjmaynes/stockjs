792ef07346f1582ef4a1979125ffc3b857aaa4fc
TJ Maynes <tjmaynes@outlook.com>
2015-03-18 12:50:58 -0400

    need to cleanup and rethink d3js candlestick example

--- js/app.js
+++ js/app.js
@@ -1,79 +1,133 @@
 'use strict';
 
-var data = [1,1,1];
+var width = 900;
+var height = 500;
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 660 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var dateFormat = d3.time.format("%Y-%m-%d");
+var end = new Date();
+var start = new Date(end.getTime() - 1000 * 60 * 60 * 24 * 60);
+var data = [];
 
-var xScale = d3.time.scale(),
-    yScale = d3.scale.linear();
+function min(a, b){ return a < b ? a : b ; }
 
-var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient('bottom')
-    .ticks(5);
+function max(a, b){ return a > b ? a : b; }    
 
-var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .orient('left');
-/*
-var series = sl.series.candlestick()
-    .xScale(xScale)
-    .yScale(yScale);
-*/
-// Create svg element
-var svg = d3.select('#candlestick').classed('candlestick', true).append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom);
+function buildChart(data){
+    var margin = 50;		   
+    
+    var chart = d3.select("#candlestick")
+	.append("svg:svg")
+	.attr("class", "candlestick")
+	.attr("width", width)
+	.attr("height", height);
+    var y = d3.scale.linear()
+	.domain([d3.min(data.map(function(x) {return x["Low"];})), d3.max(data.map(function(x){return x["High"];}))])
+	.range([height-margin, margin]);
+    var x = d3.scale.linear()
+	.domain([d3.min(data.map(function(d){return dateFormat.parse(d.Date).getTime();})),
+		 d3.max(data.map(function(d){return dateFormat.parse(d.Date).getTime();}))])
+	.range([margin,width-margin]);
 
-// Ceate chart
-var g = svg.append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    chart.selectAll("line.x")
+        .data(x.ticks(10))
+        .enter().append("svg:line")
+        .attr("class", "x")
+        .attr("x1", x)
+        .attr("x2", x)
+        .attr("y1", margin)
+        .attr("y2", height - margin)
+        .attr("stroke", "#ccc");
 
-var plotArea = g.append('g');
-plotArea.append('clipPath')
-    .attr('id', 'plotAreaClip')
-    .append('rect')
-    .attr({ width: width, height: height });
-plotArea.attr('clip-path', 'url(#plotAreaClip)');
+    chart.selectAll("line.y")
+        .data(y.ticks(10))
+        .enter().append("svg:line")
+        .attr("class", "y")
+        .attr("x1", margin)
+        .attr("x2", width - margin)
+        .attr("y1", y)
+        .attr("y2", y)
+        .attr("stroke", "#ccc");
 
-// Set scale domains
-var maxDate = d3.max(data, function (d) {
-    return d.date;
-});
+    chart.selectAll("text.xrule")
+        .data(x.ticks(10))
+        .enter().append("svg:text")
+        .attr("class", "xrule")
+        .attr("x", x)
+        .attr("y", height - margin)
+        .attr("dy", 20)
+        .attr("text-anchor", "middle")
+        .text(function(d){ var date = new Date(d * 1000);  return (date.getMonth() + 1)+"/"+date.getDate(); });
+    
+    chart.selectAll("text.yrule")
+        .data(y.ticks(10))
+        .enter().append("svg:text")
+        .attr("class", "yrule")
+        .attr("x", width - margin)
+        .attr("y", y)
+        .attr("dy", 0)
+        .attr("dx", 20)		 
+        .attr("text-anchor", "middle")
+        .text(String);
 
-xScale.domain([
+    chart.selectAll("rect")
+	.data(data)
+	.enter().append("svg:rect")
+	.attr("x", function(d) { return x(dateFormat.parse(d.Date).getTime()); })
+        .attr("y", function(d) {return y(max(d.Open, d.Close));})		  
+	.attr("height", function(d) { return y(min(d.Open, d.Close))-y(max(d.Open, d.Close));})
+	.attr("width", function(d) { return 0.5 * (width - 2*margin)/data.length; })
+        .attr("fill",function(d) { return d.Open > d.Close ? "red" : "green" ;});
 
-]);
+    chart.selectAll("line.stem")
+        .data(data)
+        .enter().append("svg:line")
+        .attr("class", "stem")
+        .attr("x1", function(d) { return x(dateFormat.parse(d.Date).getTime()) + 0.25 * (width - 2 * margin)/ data.length;})
+        .attr("x2", function(d) { return x(dateFormat.parse(d.Date).getTime()) + 0.25 * (width - 2 * margin)/ data.length;})		    
+        .attr("y1", function(d) { return y(d.High);})
+        .attr("y2", function(d) { return y(d.Low); })
+        .attr("stroke", function(d){ return d.Open > d.Close ? "red" : "green"; })
+}
 
-yScale.domain(
-    [
-        d3.min(data, function (d) {
-            return d.low;
-        }),
-        d3.max(data, function (d) {
-            return d.high;
-        })
-    ]
-).nice();
+function appendToData(x){
+    if(data.length > 0) return;
+    data = x.query.results.quote;
+    for(var i = 0 ; i < data.length; i++)
+        data[i].timestamp = (new Date(data[i].date).getTime() / 1000);
 
-// Set scale ranges
-xScale.range([0, width]);
-yScale.range([height, 0]);
+    data = data.sort(function(x, y){ return dateFormat.parse(x.Date).getTime() - dateFormat.parse(y.Date).getTime(); });			
+    buildChart(data);	  
+}
 
-// Draw axes
-g.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
+function buildQuery(name){
+    var symbol = window.location.hash;
+    if(symbol === ""){
+        symbol = name;
+    }
+    symbol = symbol.replace("#", "");		  
+    var base = "select * from yahoo.finance.historicaldata where symbol = \"{0}\" and startDate = \"{1}\" and endDate = \"{2}\"";
+    var getDateString = d3.time.format("%Y-%m-%d");
+    var query = base.format(symbol, getDateString(start), getDateString(end));
+    query = encodeURIComponent(query);		    
+    var url = "http://query.yahooapis.com/v1/public/yql?q={0}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=appendToData".format(query);
+    return url;
+}
 
-g.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis);
+function fetchData(name){	  
+    var url = buildQuery(name);		  
+    var scriptElement = document.createElement("SCRIPT");
+    scriptElement.type = "text/javascript";
+    // i add to the url the call back function
+    scriptElement.src = url;
+    document.getElementsByTagName("HEAD")[0].appendChild(scriptElement);
+}
 
-// Draw series.
-plotArea.append('g')
-    .attr('class', 'series')
-    .datum(data)
-    .call(series);
+$(document).ready(fetchData("AAPL"));
