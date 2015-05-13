537b8aca04337b01d9d9b40ab7ca7093c243a4e0
TJ Maynes <tjmaynes@outlook.com>
2015-05-12 23:23:27 -0400

    working on d3js now

--- js/app.js
+++ js/app.js
@@ -5,27 +5,32 @@ $(function(){
 	url: "http://query.yahooapis.com/v1/public/yql",
 	defaults: {
 	    symbol: "AAPL",
-	    startDate: "1990-01-01",
+	    startDate: "2014-01-01",
 	    endDate: "2015-01-01",
-	    period: "d"
+	    stockData: []
 	},
-	initialize: function(){
+	initialize: function() {
+            _.bindAll(this, "getData");
+            this.getData();
+	},
+	getData: function() {
+	    console.log(this.get("symbol"));
+	    var self = this;
 	    var data = encodeURIComponent("select * from yahoo.finance.historicaldata where symbol in ('"
 					  + this.get("symbol") + "') and startDate = \""
 					  + this.get("startDate") + "\" and endDate = \""
 					  + this.get("endDate") + "\"");
 	    $.getJSON(this.url, "q=" + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
 		.done(function (data) {
-		    //console.log("url: " + this.url + "q=" + this.data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env");
-		    //$('#result').text("Price: " + data.query.results.quote[0][0]);
-		    return data;
+		    console.log("url: " + this.url + "q=" + this.data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env");
+		    self.set({ stockData: data.query.results.quote });
 		})
 		.fail(function (jqxhr, textStatus, error) {
 		    var err = textStatus + ", " + error;
 		    console.log('Request failed: ' + err);
 		});
-        },
-	destroy: function(){
+	},
+	destroy: function() {
 	    this.model.destroy();
 	}
     });
@@ -51,104 +56,88 @@ $(function(){
 	    Backbone.View.apply(this, arguments);
 	},
 	initialize: function(options) {
-	    // create container element
-	    //_.bindAll(this, "render");
-	    this.render();
+	    _.bindAll(this, "render");
+	    candleCollection.bind('change', this.render);
 	},
-	render: function() {
-	    console.log("render chart");
-	    return;
-	    /*
-              var margin = this.options.margin;
-              this.width = this.$el.width() - margin.left - margin.right;
-              this.height = this.$el.height() - margin.top - margin.bottom;
-              var data = [];
-              var chart = d3.select("#candlestick")
-              .append("svg:svg")
-              .attr("class", "chart")
-              .attr("width", this.width)
-              .attr("height", this.height);
-              var y = d3.scale.linear()
-              .domain([d3.min(data.map(function(x) {return x["Low"];})), d3.max(data.map(function(x){return x["High"];}))])
-              .range([this.height-margin, margin]);
-              var x = d3.scale.linear()
-              .domain([d3.min(data.map(function(d){return dateFormat.parse(d.Date).getTime();})),
-              d3.max(data.map(function(d){return dateFormat.parse(d.Date).getTime();}))])
-              .range([margin,this.width-margin]);
+	render: function(options) {
+	    console.log(JSON.stringify(candleCollection));
+            var margin = this.options.margin;
+            this.width = this.$el.width() - margin.left - margin.right;
+            this.height = this.$el.height() - margin.top - margin.bottom;
+            var data =  candleCollection.toJSON(); //JSON.stringify(candleCollection);
+            var chart = d3.select("#candlestick")
+		.append("svg:svg")
+		.attr("class", "chart")
+		.attr("width", this.width)
+		.attr("height", this.height);
+            var y = d3.scale.linear()
+		.domain([d3.min(data.map(function(x) {return x["Low"];})), d3.max(data.map(function(x){return x["High"];}))])
+		.range([this.height-margin, margin]);
+            var x = d3.scale.linear()
+		.domain([d3.min(data.map(function(d){return dateFormat.parse(d.Date).getTime();})),
+			 d3.max(data.map(function(d){return dateFormat.parse(d.Date).getTime();}))])
+		.range([margin,this.width-margin]);
 
-              chart.selectAll("line.x")
-              .data(x.ticks(10))
-              .enter().append("svg:line")
-              .attr("class", "x")
-              .attr("x1", x)
-              .attr("x2", x)
-              .attr("y1", margin)
-              .attr("y2", this.height - margin)
-              .attr("stroke", "#ccc");
+            chart.selectAll("line.x")
+		.data(x.ticks(10))
+		.enter().append("svg:line")
+		.attr("class", "x")
+		.attr("x1", x)
+		.attr("x2", x)
+		.attr("y1", margin)
+		.attr("y2", this.height - margin)
+		.attr("stroke", "#ccc");
 
-              chart.selectAll("line.y")
-              .data(y.ticks(10))
-              .enter().append("svg:line")
-              .attr("class", "y")
-              .attr("x1", margin)
-              .attr("x2", this.width - margin)
-              .attr("y1", y)
-              .attr("y2", y)
-              .attr("stroke", "#ccc");
+            chart.selectAll("line.y")
+		.data(y.ticks(10))
+		.enter().append("svg:line")
+		.attr("class", "y")
+		.attr("x1", margin)
+		.attr("x2", this.width - margin)
+		.attr("y1", y)
+		.attr("y2", y)
+		.attr("stroke", "#ccc");
 
-              chart.selectAll("text.xrule")
-              .data(x.ticks(10))
-              .enter().append("svg:text")
-              .attr("class", "xrule")
-              .attr("x", x)
-              .attr("y", this.height - margin)
-              .attr("dy", 20)
-              .attr("text-anchor", "middle")
-              .text(function(d){ var date = new Date(d * 1000);  return (date.getMonth() + 1)+"/"+date.getDate(); });
+            chart.selectAll("text.xrule")
+		.data(x.ticks(10))
+		.enter().append("svg:text")
+		.attr("class", "xrule")
+		.attr("x", x)
+		.attr("y", this.height - margin)
+		.attr("dy", 20)
+		.attr("text-anchor", "middle")
+		.text(function(d){ var date = new Date(d * 1000);  return (date.getMonth() + 1)+"/"+date.getDate(); });
 
-              chart.selectAll("text.yrule")
-              .data(y.ticks(10))
-              .enter().append("svg:text")
-              .attr("class", "yrule")
-              .attr("x", this.width - margin)
-              .attr("y", y)
-              .attr("dy", 0)
-              .attr("dx", 20)
-              .attr("text-anchor", "middle")
-              .text(String);
+            chart.selectAll("text.yrule")
+		.data(y.ticks(10))
+		.enter().append("svg:text")
+		.attr("class", "yrule")
+		.attr("x", this.width - margin)
+		.attr("y", y)
+		.attr("dy", 0)
+		.attr("dx", 20)
+		.attr("text-anchor", "middle")
+		.text(String);
 
-              chart.selectAll("rect")
-              .data(data)
-              .enter().append("svg:rect")
-              .attr("x", function(d) { return x(dateFormat.parse(d.Date).getTime()); })
-              .attr("y", function(d) {return y(max(d.Open, d.Close));})
-              .attr("this.height", function(d) { return y(min(d.Open, d.Close))-y(max(d.Open, d.Close));})
-              .attr("this.width", function(d) { return 0.5 * (this.width - 2*margin)/data.length; })
-              .attr("fill",function(d) { return d.Open > d.Close ? "red" : "green" ;});
+            chart.selectAll("rect")
+		.data(data)
+		.enter().append("svg:rect")
+		.attr("x", function(d) { return x(dateFormat.parse(d.Date).getTime()); })
+		.attr("y", function(d) {return y(max(d.Open, d.Close));})
+		.attr("this.height", function(d) { return y(min(d.Open, d.Close))-y(max(d.Open, d.Close));})
+		.attr("this.width", function(d) { return 0.5 * (this.width - 2*margin)/data.length; })
+		.attr("fill",function(d) { return d.Open > d.Close ? "red" : "green" ;});
 
-              chart.selectAll("line.stem")
-              .data(data)
-              .enter().append("svg:line")
-	      .attr("class", "stem")
-              .attr("x1", function(d) { return x(dateFormat.parse(d.Date).getTime()) + 0.25 * (this.width - 2 * margin)/ data.length;})
-              .attr("x2", function(d) { return x(dateFormat.parse(d.Date).getTime()) + 0.25 * (this.width - 2 * margin)/ data.length;})
-              .attr("y1", function(d) { return y(d.High);})
-              .attr("y2", function(d) { return y(d.Low); })
-              .attr("stroke", function(d){ return d.Open > d.Close ? "red" : "green"; })
-              return this;
-	    */
-	}
-    });
-
-    var ListView = Backbone.View.extend({
-	initialize: function() {
-	    //this.model.bind('click', this.render); // test this later --> may effect new on line 184
-	    this.render();
-	},
-	render: function() {
-	    //this.model.fetch();
-	    this.$el.html(JSON.stringify(candleCollection));
-	    console.log(candleCollection.toJSON());
+            chart.selectAll("line.stem")
+		.data(data)
+		.enter().append("svg:line")
+		.attr("class", "stem")
+		.attr("x1", function(d) { return x(dateFormat.parse(d.Date).getTime()) + 0.25 * (this.width - 2 * margin)/ data.length;})
+		.attr("x2", function(d) { return x(dateFormat.parse(d.Date).getTime()) + 0.25 * (this.width - 2 * margin)/ data.length;})
+		.attr("y1", function(d) { return y(d.High);})
+		.attr("y2", function(d) { return y(d.Low); })
+		.attr("stroke", function(d){ return d.Open > d.Close ? "red" : "green"; })
+            return this;
 	}
     });
 
@@ -190,26 +179,20 @@ $(function(){
 	    candleCollection.create(new CandleModel());
 
 	    // create new view objects
-	    var candleView = new CandleView({ el: $("#candleView") });
-	    var listView = new ListView({ el: $("#listView") });
+	    new CandleView({ el: $("#candleView") });
 	},
 	build: function() {
 	    var startField = this.$( "#startDatePicker" ).val();
 	    var endField = this.$( "#endDatePicker" ).val();
-	    var symbolField =  this.$('#symbolField').val();
-	    var periodField = $('option[name="periodOption"]:checked').val();
-	    var exchangeField = this.$('input[name="exchangeOption"]:checked').val();
-	    if (symbolField != '' && periodField != '' && startField != '' && endField != '') {
-		var buildNewModel = new CandleModel({ symbol: symbolField, startDate: startField, endDate: endField, period: periodField });
-		candleCollection.create(buildNewModel);
-		var candleView = new CandleView({ el: $("#candleView") });
-		var listView = new ListView({ el: $("#listView") });
-	    }
-	    else if (symbolField == '') {
-		$(".symbolField").removeAttr('placeholder');
-		$(".symbolField").attr('placeholder','Please enter a valid ticker symbol!');
-	    } else {
+	    var symbolField =  this.$( "#symbolField" ).val();
 
+	    if (symbolField != "" && startField != "" && endField != "") {
+		var buildNewModel = new CandleModel({ symbol: symbolField, startDate: startField, endDate: endField });
+		candleCollection.create(buildNewModel);
+		new CandleView({ el: $("#candleView") });
+	    } else {
+		$("#symbolField").removeAttr('placeholder');
+		$("#symbolField").attr('placeholder','Please enter a valid ticker symbol!');
 	    }
 	},
 	reset: function() {
